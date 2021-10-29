3818c20e556bb355d1778ebcea379b54624e00c6
Linuxpert <g.manfre10@gmail.com>
2021-10-29 16:51:12 +0200

    esercizio palindromo completato

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,19 +1,39 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Dare l’output relativo.
+
+
+//div per stampare i le frasi in html
+const parola = document.getElementById("parola");
+const result = document.getElementById("result");
+
+//chiedere all'utente la parola
+var parolaInserita = prompt("inserisci una parola");
+parola.innerHTML = "la parola inserita è: " + parolaInserita;
+
+var parolaInversa = invertiParola(parolaInserita);
+
+//comparare la parola inserita con quella inversa
+if(comparaParola(parolaInserita, parolaInversa)){
+    result.innerHTML = "si la parola " + parolaInserita +  " è palindroma "
+    console.log("si la parola è palindroma");
+}else {
+    result.innerHTML = "no la parola " + parolaInserita +  " non è palindroma "
+    console.log("no la parola non è palindroma");
+}
+
+const pali = comparaParola(parolaInserita, parolaInversa);
 
 
 
-let isPalindrome = prompt("scrivi una parola palindroma")
 
-    isPalindrome = function(str) {
-    var strLen = str.length;
-    if (strLen === 0 || strLen === 1) {
-        return true;
-    }
-    
-    if (str[0] === str[strLen - 1]) {
-        return isPalindrome( str.slice(1, strLen - 1) );
-    }
-    
-    return false;
-};
+//funzioni
+function invertiParola(stringa){
+    var stringaInversa = stringa.split('').reverse().join('');
+    return stringaInversa;
+}
 
-console.log(isPalindrome('madam'));
+function comparaParola(stringa1, stringa2){
+    var controllo = stringa1 === stringa2;
+    return controllo;
+}
